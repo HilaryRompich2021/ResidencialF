ace7e8c6f0f1e1f8235557859856559fb4763361
Hilary Rompich <hrompichp1@miumg.edu.gt>
2025-06-12 17:26:57 -0600

    prueba en el environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api'
+  //apiUrl: 'http://localhost:8080/api'
+  apiUrl: 'https://micasitaseguraresidencial-g3bed6dzbab9c2a4.canadacentral-01.azurewebsites.net/api'
+
 };
